50fe165620dc429701bce56a075e63e8526797bf
Kyle Spraggs <kyle.spraggs@gmail.com>
2015-08-05 12:44:35 -0500

    Add autoloading

--- bin/tonis.php
+++ bin/tonis.php
@@ -1,2 +1,13 @@
 <?php
-echo 'wat';
+
+$autoloadFiles = [
+    __DIR__ . '/../vendor/autoload.php',
+    __DIR__ . '/../../../autoload.php'
+];
+
+foreach ($autoloadFiles as $file) {
+    if (file_exists($file)) {
+        require_once $file;
+    }
+}
+
